6d8e7220300aa2254e4f5a60676d3de044b316c9
李一一 <47694713+Li-YiYi@users.noreply.github.com>
2019-08-14 22:42:14 +0800

    Update Button_BLE.ino

--- examples/Blinker_Button/Button_BLE/Button_BLE.ino
+++ examples/Blinker_Button/Button_BLE/Button_BLE.ino
@@ -1,14 +1,15 @@
 #define BLINKER_BLE
 
 #include <Blinker.h>
+#include <Servo.h>
 
 #define BUTTON_1 "ButtonKey"
 
 BlinkerButton Button1(BUTTON_1);
-
+Servo myservo;
 
 void servopulse(int angle){
-    int pulsewidth=(angle*11)+500;  //将角度转化为500-2480的脉宽值，每多转1度，对应高电平多11us
+    int pulsewidth=(angle*11)+500;  //将角度转化为500-2480的脉宽值，每多转1度，对应高电平多11us     digitalWrite(servopin,HIGH);    //将舵机接口电平至高
     digitalWrite(11,HIGH); 
     delayMicroseconds(pulsewidth);  //延时脉宽值的微秒数
     digitalWrite(11,LOW);     //将舵机接口电平至低
@@ -25,18 +26,14 @@ void button1_callback(const String & state)
     else if (state == BLINKER_CMD_ON) {
         BLINKER_LOG("Toggle on!");
         Button1.print("on");
-        for(int i=0;i<50;i++)
-        {
-          servopulse(165);
-        }   
+        myservo.write(165);
+        Blinker.delay(25);
     }
     else if (state == BLINKER_CMD_OFF) {
         BLINKER_LOG("Toggle off!");
         Button1.print("off");
-        for(int i=0;i<50;i++)
-        {
-          servopulse(15);
-        }   
+        myservo.write(15);
+        Blinker.delay(25);
     }
     else {
         BLINKER_LOG("Get user setting: ", state);
@@ -45,13 +42,14 @@ void button1_callback(const String & state)
 
 void setup()
 {
-    Serial.begin(115200);
-    BLINKER_DEBUG.stream(Serial);
+    //Serial.begin(115200);
+    //BLINKER_DEBUG.stream(Serial);
 
     pinMode(11, OUTPUT);
 
-    Blinker.begin();
+    Blinker.begin(0,1);
     Button1.attach(button1_callback);
+    myservo.attach(11);
 }
 
 void loop()
