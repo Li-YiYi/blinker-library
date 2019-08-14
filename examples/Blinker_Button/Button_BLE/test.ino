696647d599e805002d60faba7e43de4bdfb2c13c
李一一 <47694713+Li-YiYi@users.noreply.github.com>
2019-08-14 20:32:42 +0800

    Create test.ino

---
+++ examples/Blinker_Button/Button_BLE/test.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+Servo servo;
+
+
+
+
+
+
+
+
+void setup(){
+
+	servo.attach(11);
+
+}
+
+void loop() {
+    servo.write(20);
+    delay(1000);
+    servo.write(150);
+    delay(1000);
+
+}
